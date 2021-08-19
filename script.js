0f4f12a977ea8bed390ff5e2895d9fc8e693ec62
Alex Vilentchuk <avilentchuk@gmail.com>
2021-08-19 22:43:50 +0300

    fixed file structure, import list and class names correspoding to BEM.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 let editWindow = document.querySelector(".edit-window");
-let editButton = document.querySelector(".button__edit");
+let editButton = document.querySelector(".button_edit");
 
 // console.log(editWindow.style.visibility);
 // console.log(editButton);
 function openEdit() {
-  let submitButton = editWindow.querySelector('.button__submit');
-  let closeButton = editWindow.querySelector('.button__close');
+  let submitButton = editWindow.querySelector('.button_submit');
+  let closeButton = editWindow.querySelector('.button_close');
   let profileName = document.querySelector('.profile__name');
   let profileJob = document.querySelector('.profile__about');
   let editName = document.querySelector('.edit-window__name');
@@ -15,17 +15,17 @@ function openEdit() {
   editJob.value = profileJob.textContent;
 
   editWindow.style.visibility = "visible";
-  document.querySelector('.edit-window__mask').style.visibility = "visible";
+  document.querySelector('.page__mask').style.visibility = "visible";
 
   closeButton.addEventListener('click', function () {
-    document.querySelector('.edit-window__mask').style.visibility = "hidden";
+    document.querySelector('.page__mask').style.visibility = "hidden";
     editWindow.style.visibility = "hidden";
   });
   submitButton.addEventListener('click',function () {
     profileName.textContent = editName.value;
     profileJob.textContent = editJob.value;
     editWindow.style.visibility = "hidden";
-    ocument.querySelector('.edit-window__mask').style.visibility = "hidden";
+    document.querySelector('.page__mask').style.visibility = "hidden";
   })
 }
 
