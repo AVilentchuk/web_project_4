7dfb5bbd7ee09033897d1ef7423d5e7747fd381b
Alex Vilentchuk <avilentchuk@gmail.com>
2021-08-22 20:57:49 +0300

    2nd revision fixes, added new functionality for overflow.

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-let editButton = document.querySelector(".button_edit");
-let submitButton = document.querySelector(".button_submit");
-let closeButton = document.querySelector(".edit-window__button-close");
+let editButton = document.querySelector(".button_profile_edit");
+let submitButton = document.querySelector(".button_popup_submit");
+let closeButton = document.querySelector(".button_popup_close");
 let popupBack = document.querySelector(".popup");
-let form = document.querySelector(".edit-window__form");
+let form = document.querySelector(".popup__form");
 let curName = document.querySelector(".profile__name");
 let curJob = document.querySelector(".profile__job");
 let newName = document.getElementById("name");
 let newJob = document.getElementById("job-des");
 
 editButton.addEventListener("click", openEdit);
-newName.value = curName.textContent;
-newJob.value = curJob.textContent;
+
+function pullProfData() {
+  newName.value = curName.textContent;
+  newJob.value = curJob.textContent;
+}
+
+function closePop() {
+  popupBack.classList.remove("popup_active");
+}
+function openPop() {
+  popupBack.classList.add("popup_active");
+}
 
 function openEdit() {
-
-  popupBack.classList.add("popup_active");
+  pullProfData();
+  openPop();
   closeButton.addEventListener("click", function closeClick() {
     popupBack.classList.remove("popup_active");
-    closeButton.removeEventListener("click", closeClick);
   });
 
-  form.addEventListener('submit', function (e) {
-    e.preventDefault()
-     curName.textContent = newName.value;
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    curName.textContent = newName.value;
     curJob.textContent = newJob.value;
-    popupBack.classList.remove("popup_active")
+    closePop();
   });
 }
 
-
+// additional adaptability
+let cardTxt = document.querySelectorAll(".card__text");
+cardTxt.forEach((e) => {
+  if (e.scrollWidth > e.clientWidth) {
+    e.nextElementSibling.nextElementSibling.innerHTML = e.innerHTML;
+    console.log(e.nextSibling.innerText);
+  }
+});
