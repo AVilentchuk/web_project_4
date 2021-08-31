50a2ac841753b94fefaa9c92571f7991528a7fed
Alex Vilentchuk <avilentchuk@gmail.com>
2021-08-31 18:02:31 +0300

    fixed JS implemented minor improvements in CSS attributs.

--- scripts/index.js
+++ scripts/index.js
@@ -1,159 +1,150 @@
+// reused variables ( additional variables exist in function to be used in them exclusively )
+// buttons
 const editButton = document.querySelector(".profile__button-edit");
-const submitButton = document.querySelectorAll(".button_type_submit");
-const closeButton = document.querySelectorAll(".button_type_close");
 const addButton = document.querySelector(".profile__button-add");
-const popupBack = document.querySelector(".popup");
-const form = document.querySelectorAll(".popup__form");
-const curName = document.querySelector(".profile__name");
-const curJob = document.querySelector(".profile__job");
+//popup background
+const popupWrapper = document.querySelector(".popup");
+//profile attributes
+const currentName = document.querySelector(".profile__name");
+const currentJob = document.querySelector(".profile__job");
+//profile input field
 const newName = document.getElementById("name");
 const newJob = document.getElementById("job-des");
+//Add input fields
+const newPlace = document.getElementById("place-title");
+const newImageLink = document.getElementById("image-link");
+//Image popup window attributes
+const imageWindowName = document.querySelector(".popup__place-name");
+const imageWindowPhoto = document.querySelector(".popup__img");
+//template
+const cardTemplate = document
+  .querySelector("#card-temp")
+  .content.querySelector(".card");
 
-editButton.addEventListener("click", openEdit);
-addButton.addEventListener("click", openAdd);
-// Array of initial cards
-let initialCards = [
-  {
-    name: "Yosemite Valley",
-    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
-  },
-  {
-    name: "Lake Louise",
-    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
-  },
-  {
-    name: "Bald Mountains",
-    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
-  },
-  {
-    name: "Latemar",
-    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
-  },
-  {
-    name: "Vanoise National Park",
-    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
-  },
-  {
-    name: "Lago di Braies",
-    link: "https://code.s3.yandex.net/web-code/lago.jpg",
-  },
-];
-//Card add constructor
-const cardAdd = (locName, imgUrl) => {
-  const cardTemp = document.querySelector("#card-temp").content;
-  const newCard = cardTemp.querySelector(".card").cloneNode(true);
-  const inLocName = newCard.querySelector(".card__text");
-  const inImgUrl = newCard.querySelector(".card__image");
-  inLocName.innerText = locName;
-  inImgUrl.src = imgUrl;
-  document.querySelector(".locations").append(newCard);
-  let cardTxt = document.querySelectorAll(".card__text");
-  cardTxt.forEach((e) => {
-    if (e.scrollWidth > e.clientWidth) {
-      e.nextElementSibling.nextElementSibling.innerHTML = e.innerHTML;
-      console.log(e.nextSibling.innerText);
-    }
-  });
-};
-//loop array constructor
-initialCards.forEach((location) => {
-  console.log(location);
-  const locName = location.name;
-  const imgUrl = location.link;
-  console.log(locName);
-  console.log(imgUrl);
-  cardAdd(locName, imgUrl);
-});
-//base page button listeners
-editButton.addEventListener("click", openEdit);
-addButton.addEventListener("click", openAdd);
-
-// delBtn.addEventListener("click", function print() {
-//   console.log("lol");
-// });
-
-//functions
-
-function pullProfData() {
-  newName.value = curName.textContent;
-  newJob.value = curJob.textContent;
-}
-
-function closePop(e) {
-  popupBack.classList.remove("popup_active");
-  e.currentTarget.parentNode.style.display = "none";
-}
-
-function openPop(child) {
-  const popWin = popupBack.querySelector(child);
-  popupBack.classList.add("popup_active");
-  popWin.style.display = "flex";
-}
-
-function submitProf(e) {
-  e.preventDefault();
-  curName.textContent = newName.value;
-  curJob.textContent = newJob.value;
-  closePop(e);
-}
-function sumbitPlc(e) {
-  e.preventDefault();
-  const plcTitle = document.getElementById("place-title").value;
-  const imgUrl = document.getElementById("image-link").value;
-  cardAdd(plcTitle, imgUrl);
-  document.getElementById("place-title").value = "";
-  document.getElementById("image-link").value = "";
-  closePop(e);
-}
-
-function openEdit() {
-  pullProfData();
-  openPop("#w-edit");
-  closeButton.forEach((btn) => {
-    btn.addEventListener("click", closePop);
-    document
-      .getElementsByName("editWindow")[0]
-      .addEventListener("submit", submitProf);
-  });
-}
-function openAdd() {
-  openPop("#w-add");
-  closeButton.forEach((btn) => {
-    btn.addEventListener("click", closePop);
-    document
-      .getElementsByName("addWindow")[0]
-      .addEventListener("submit", sumbitPlc);
-  });
-}
-function openImg() {
-  openPop("#w-img");
-
-  closeButton.forEach((btn) => {
-    btn.addEventListener("click", closePop);
-  });
-}
-
-const delBtn = document.querySelectorAll(".button_type_delete");
-delBtn.forEach((btn) => {
-  btn.addEventListener("click", (e) => {
-    e.currentTarget.parentNode.remove();
-  });
-});
-const cards = document.querySelectorAll(".card");
-
-cards.forEach((card) => {
-  const cardPhoto = card.querySelector(".card__image");
-  console.log(cardPhoto);
-  const cardTitle = card.querySelector(".card__text");
-
-  cardPhoto.addEventListener("click", () => {
-    console.log(cardPhoto.getAttribute("src"));
-    document.querySelector(".popup__img").src = cardPhoto.getAttribute("src");
-    console.log(document.querySelector(".popup__img").getAttribute("src"));
-    document.querySelector(".popup__place-name").innerHTML =
-      cardTitle.innerHTML;
+//<<START>> function to construct new cards followed by loop to run it on each item in initial array <<START>>
+const addCard = (locationName, imageUrl) => {
+  // template variables used in this function
+  const newCard = cardTemplate.cloneNode(true);
+  const inputLocationName = newCard.querySelector(".card__text");
+  const inputImageUrl = newCard.querySelector(".card__image");
+  const deleteButton = newCard.querySelector(".button_type_delete");
+  // value assignment here
+  inputLocationName.textContent = locationName;
+  inputImageUrl.src = imageUrl;
+  inputImageUrl.alt = `Photograph of ${locationName}`;
+  //creating listener for image
+  inputImageUrl.addEventListener("click", () => {
+    imageWindowName.textContent = locationName;
+    imageWindowPhoto.src = imageUrl;
     openImg();
   });
-});
+  //creating delete button listener
+  deleteButton.addEventListener("click", (evt) => {
+    evt.currentTarget.parentNode.remove();
+  });
+  //attaching the card at the end of locations.
+  document.querySelector(".locations").prepend(newCard);
+  //this adds a caption to the place's name if it doesn't fit in the card.
+  if (inputLocationName.scrollWidth > inputLocationName.clientWidth) {
+    inputLocationName.nextElementSibling.nextElementSibling.textContent =
+      inputLocationName.textContent;
+  }
+};
 
-// alt for del onclick="return this.parentNode.remove();"
+//Looping over the array in initialCards.js
+initialCards.forEach((location) => {
+  const locationName = location.name;
+  const imageUrl = location.link;
+  addCard(locationName, imageUrl);
+});
+//<<END>> function to construct new cards followed by loop to run it on each item in initial array <<END>>
+
+//<<START>> functions for opening & closing windows <<START>>
+//pulls current name and job from the profile and updates it into the inputs - for openEdit()
+function pullProfData() {
+  newName.value = currentName.textContent;
+  newJob.value = currentJob.textContent;
+}
+//opens edit window
+function openEdit() {
+  pullProfData();
+  windowId = "#w-edit";
+  openPopup(windowId);
+  const currentWindow = popupWrapper.querySelector(windowId);
+  windowCloseButton = currentWindow.querySelector(".button_type_close");
+  windowCloseButton.addEventListener("click", closePopup);
+  windowForm = currentWindow.querySelector(".popup__form");
+  windowForm.addEventListener('submit',submitProfile);
+  };
+
+//opens add window
+function openAdd() {
+  windowId = "#w-add";
+  openPopup(windowId);
+  const currentWindow = popupWrapper.querySelector(windowId);
+  windowCloseButton = currentWindow.querySelector(".button_type_close");
+  windowCloseButton.addEventListener("click", closePopup);
+  windowForm = currentWindow.querySelector(".popup__form");
+  windowForm.addEventListener('submit',submitPlace);
+}
+//opens clicked image.
+function openImg() {
+  windowId = "#w-img";
+  openPopup(windowId);
+  const currentWindow = popupWrapper.querySelector(windowId);
+  windowCloseButton = currentWindow.querySelector(".button_type_close");
+  windowCloseButton.addEventListener("click", closePopup);
+}
+//closes popup and active window/windows.
+function closePopup(e) {
+  popupWrapper.classList.remove("popup_active");
+  e.currentTarget.parentNode.style.visibility = "collapse";
+  e.target.removeEventListener('click', closePopup);
+}
+//function to make transitioned items un-clickable during transition
+function unClickAbility(item) {
+  item.addEventListener("transitionstart", function countDown(e) {
+    var delayTime = getComputedStyle(e.target)[["transitionDuration"]];
+    delayTime = parseFloat(delayTime) * 1000;
+    e.target.style.pointerEvents = "none";
+    setTimeout(function () {
+      e.target.style.pointerEvents = "all";
+      item.removeEventListener("transitionstart", countDown);
+    }, delayTime);
+  });
+}
+//Opens the popup background and selected window/windows.
+function openPopup(child) {
+  const popupWindow = popupWrapper.querySelector(child);
+  popupWrapper.classList.add("popup_active");
+  unClickAbility(popupWrapper);
+  popupWindow.style.visibility = "visible";
+}
+//<<END>> functions for opening windows <<END>>
+
+//<<START>> base page button listeners <<START>>
+editButton.addEventListener("click", openEdit); //edit button listener
+addButton.addEventListener("click", openAdd); //add button listener
+//<<END>> base page button listeners <<END>>
+
+//<<START>> Submission functions <<START>>
+function submitProfile(e) {
+  //submits the changes done to the profile in the edit window.
+  e.preventDefault(); //prevent default behavior or submit form.
+  currentName.textContent = newName.value;
+  currentJob.textContent = newJob.value;
+  e.target.removeEventListener("submit",submitProfile);
+  closePopup(e);
+}
+function submitPlace(e) {
+  //submits the new location added in the add window.
+  e.preventDefault(); //prevent default behavior or submit form.
+  const placeTitle = newPlace.value; // get current values inside
+  const imageUrl = newImageLink.value; // input field of the add window.
+  addCard(placeTitle, imageUrl);
+  newPlace.value = ""; // empties the
+  newImageLink.value = ""; // input fields.
+  e.target.removeEventListener('submit',submitPlace);
+  closePopup(e);
+}
+//<<END>> Submission functions <<END>>
